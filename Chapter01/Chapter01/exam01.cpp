33f018dd1ca007c6f0c3d9cd33dfde95b943db0f
mbshin <mbshin@swm.ai>
2024-01-11 08:50:57 +0900

    feat: #1 초기 프로젝트 생성
    close #1#

---
+++ Chapter01/Chapter01/exam01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//int main(void) {
+//	cout << "나의 첫번째 C++프로그램" << endl;
+//	printf("안녕하세요\n");
+//
+//	int age = 0;
+//	cout << "변수 age의 주소 : " << &age << endl;
+//
+//	string s1 = "안녕하세요";
+//	string s2 = "반갑습니다";
+//
+//	string s3 = s1 + s2 + "!!!";
+//	cout << s3 << endl << endl;
+//
+//	cout << "변수 s1의 주소 : " << &s1 << endl;
+//	cout << "변수 s2의 주소 : " << &s2 << endl;
+//	cout << "변수 s3의 주소 : " << &s3 << endl;
+//
+//	//return 0;
+//}
---
+++ Chapter01/Chapter01/exam05.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//#include "exam05.h"
+//
+//using namespace std;
+//
+//int main() {
+//	int a = 5;
+//	PrintVal(a);
+//	PrintRef(a);
+//	PrintConstRef(a);
+//	PrintAddress(&a);
+//}
+//
+//void PrintVal(int a) {
+//	//a = 10;
+//	cout << a << endl;
+//}
+//
+//void PrintRef(int& a) {
+//	//a = 10;
+//	cout << a << endl;
+//}
+//
+//void PrintConstRef(const int& a) {
+//	//a = 10;
+//	cout << a << endl;
+//}
+//void PrintAddress(int* a) {
+//	cout << *a << endl;
+//}
