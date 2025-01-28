c7c0f7ca55e2deadc77cb4f03172bd1963dd6761
ShinMinBeom <alsqja17@naver.com>
2025-01-29 01:32:23 +0900

    feat: #7 casting 추가
    close #7

---
+++ casting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/**
+ * 캐스팅
+ * 1) static_cast
+ * 2) dynamic_cast = 상속 관계에서 안전 형변환(하나라도 virtual 함수를 만들면 컴파일이 통과된다)
+ * 하지만 잘못된 타입으로 캐스팅했다면 nullptr을 반환함함
+ * 3) const_cast
+ * 4) reinterpret_cast
+ */
+
+int main() {
+    return 0;
+}
